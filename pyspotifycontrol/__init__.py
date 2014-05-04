e9efb24ec51736429e4580143aa4c9b4d1a44193
Christopher Su <christophersu9@gmail.com>
2014-05-03 19:53:20 -0700

    moving pyspotifycontrol to a module

--- pyspotifycontrol/__init__.py
+++
@@ -1,1 +0,0 @@
-import spotify_control
--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='pyspotifycontrol',
-    version='1.0.0',
-    description='Controls Spotify for Mac using AppleScripts through Python.',
-    author='Christopher Su',
-    packages=['pyspotifycontrol']
-    )
